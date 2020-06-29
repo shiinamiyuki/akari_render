3ad38de683b4d1afd3f9b17eb39b142af85a0a18
椎名深雪 <xiaochun.tong@mail.utoronto.ca>
2020-06-29 00:08:51 -0400

    restore ui folder

---
+++ src/app/ui/window_context.h
@@ -0,0 +1,50 @@
+// MIT License
+//
+// Copyright (c) 2020 椎名深雪
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef AKARIRENDER_WINDOWCONTEXT_H
+#define AKARIRENDER_WINDOWCONTEXT_H
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
+#include <memory>
+
+namespace akari::Gui {
+    class GLFWContext {
+      public:
+        GLFWContext();
+        ~GLFWContext();
+    };
+
+    class Window {
+      protected:
+        GLFWwindow *window = nullptr;
+
+      public:
+        Window(GLFWContext &context);
+        virtual void Show() = 0;
+        ~Window();
+    };
+    std::shared_ptr<Window> CreateAppWindow(GLFWContext &context);
+} // namespace akari::Gui
+#endif // AKARIRENDER_WINDOWCONTEXT_H
