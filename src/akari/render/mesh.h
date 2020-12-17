7ef018e9d9b4b69bdaa8535095c8d3a640160acb
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2020-12-17 02:32:40 -0500

    integrating nano-akari

--- src/akari/render/mesh.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <akari/render/instance.h>
-namespace akari::render {
-    class MeshNode : public ShapeNode {
-      public:
-        virtual MeshInstance create_instance(Allocator<>) = 0;
-    };
-} // namespace akari::render
