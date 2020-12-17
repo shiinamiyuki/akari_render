6053c90d7fbdf6b414dd0cd218b09d708af77069
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2020-12-17 17:20:50 -0500

    does this work?

--- scripts/build-linux-cpu.sh
+++ scripts/build-linux-cpu.sh
@@ -4,5 +4,6 @@ git clone https://github.com/microsoft/vcpkg --depth 1
 cd vcpkg
 ./bootstrap-vcpkg.sh
 cd ..
-cmake .. -DCMAKE_BUILD_TYPE=Release -GNinja -DDCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake\
+     -DCMAKE_MAKE_PROGRAM=make -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
 ninja
