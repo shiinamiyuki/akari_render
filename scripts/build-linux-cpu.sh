310a6b4bf33d1639567b1efdb5890c9719ab336d
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2020-12-17 18:16:11 -0500

    update ci

--- scripts/build-linux-cpu.sh
+++ scripts/build-linux-cpu.sh
@@ -3,6 +3,12 @@ cd build-linux-cpu
 git clone https://github.com/microsoft/vcpkg --depth 1
 cd vcpkg
 ./bootstrap-vcpkg.sh
+./vcpkg update
+export VCPKG_DEFAULT_TRIPLET=x64-linux-rel
+# prevent travis from terminating our job
+./vcpkg install embree3:x64-linux
+./vcpkg install openimageio:x64-linux
+./vcpkg install assimp:x64-linux
 cd ..
 cmake .. -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake\
      -DCMAKE_MAKE_PROGRAM=make -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
